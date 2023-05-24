7238ad94da5eb2f531a5d9a6c2dfdf6ec25af966
alangorgin <127779778+alangorgin@users.noreply.github.com>
2023-05-24 12:01:24 +0200

    got weather info from API and rendered on page

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,33 @@
 const apiKey = "262cd0cb3667ea5580cc6eb3a24dead2";
 
-
 // User searches for a location
 // We take the search term and send it to the weather API
 // We breakdown the received data and display it on the site
 
 const searchButton = document.querySelector("button");
+const tempElement = document.querySelector("#h2-main");
+const weatherDescription = document.querySelector("#h4-main");
+const footer = document.querySelector("#h2-footer");
 
 searchButton.addEventListener("click", getAPI);
 
-function getAPI(){
-    // Get user input
-    const searchBar = document.querySelector("#searchbar");
-    const searchPhrase = searchBar.value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchPhrase}&APPID=${apiKey}`;
-    fetch(url);
+async function getAPI() {
+  const searchBar = document.querySelector("#searchbar");
+  const searchPhrase = searchBar.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchPhrase}&APPID=${apiKey}`;
+  const response = await fetch(url);
+  const responseBody = await response.json();
+  const celciusFeelsLike = responseBody.main.feels_like - 273.15;
 
+  const celcius = responseBody.main.temp - 273.15;
+  tempElement.textContent = `The temperature in ${
+    responseBody.name
+  } is ${Math.round(celcius)}°C but feels like ${Math.round(
+    celciusFeelsLike
+  )}°C.`;
 
+  weatherDescription.textContent =
+    responseBody.weather[0].description + " vibes.";
+
+  footer.textContent = `Humidity: ${responseBody.main.humidity} | Pressure: ${responseBody.main.pressure} | Wind: ${responseBody.wind.speed}`;
 }
